117d3f560b89c08a864847a81158a268424919b4
cedric.nanni@aprilsoftware.com <cedric.nanni@aprilsoftware.com>
2025-11-18 22:03:01 +0100

    Schema Synchronizer

--- norm4j-test/src/test/resources/db/v0.1/oracle/ddl.sql
+++ norm4j-test/src/test/resources/db/v0.1/oracle/ddl.sql
@@ -1,10 +1,10 @@
 CREATE TABLE book (id NUMBER(10), name VARCHAR2(255) NOT NULL, author_id NUMBER(10) NOT NULL, PRIMARY KEY (id));
 
+CREATE TABLE bookorderitem (id NUMBER(10), order_id NUMBER(10) NOT NULL, book_id NUMBER(10) NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE author (id NUMBER(10), name VARCHAR2(255) NOT NULL, PRIMARY KEY (id));
 
 CREATE TABLE bookorder (id NUMBER(10), orderDate TIMESTAMP NOT NULL, PRIMARY KEY (id));
-
-CREATE TABLE bookorderitem (id NUMBER(10), order_id NUMBER(10) NOT NULL, book_id NUMBER(10) NOT NULL, PRIMARY KEY (id));
 
 ALTER TABLE book ADD CONSTRAINT fk_book_author FOREIGN KEY (author_id) REFERENCES author (id);
 
